c3d442e5aa85572832078b615976d839ba717814
Fotis Tsalampounis <fsketo@gmail.com>
2017-04-20 00:45:40 +0300

    Fixed json format bug

--- radialTreemap/jsonParse.py
+++ radialTreemap/jsonParse.py
@@ -5,10 +5,14 @@ with open('radial1.json') as data_file:
 newFile=open('radial.json','w')
 
 for i in range(len(data)):
-    if i is not 1:
+    if i is not 0:
         curLink = data[i]
         curLink["referer"] =curLink["referer"][2:]
         curLink["referer"] =curLink["referer"][:len(curLink["referer"])-1]
         data[i]=curLink
-newFile.write(str(data))
+data[0]=""
+temp=str(data)
+temp=temp[5:]
+temp="["+temp
+newFile.write(temp)
 newFile.close
