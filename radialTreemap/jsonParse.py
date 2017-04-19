06e42ca85055c59fc0a3c3601abbbb7bf4251c74
Fotis Tsalampounis <fsketo@gmail.com>
2017-04-19 23:28:25 +0300

    jsonParse reformats the json file into the correct form

--- radialTreemap/jsonParse.py
+++ radialTreemap/jsonParse.py
@@ -1,7 +1,14 @@
 import json
-from pprint import pprint
 
-with open('radial.json') as data_file:    
+with open('radial1.json') as data_file:    
     data = json.load(data_file)
+newFile=open('radial.json','w')
 
-pprint(data)
+for i in range(len(data)):
+    if i is not 1:
+        curLink = data[i]
+        curLink["referer"] =curLink["referer"][2:]
+        curLink["referer"] =curLink["referer"][:len(curLink["referer"])-1]
+        data[i]=curLink
+newFile.write(str(data))
+newFile.close
