22abe4aa29dfccf21d6a9504551a2a3c375f70f3
Fotis Tsalampounis <fsketo@gmail.com>
2017-04-21 16:23:52 +0300

    Added archived link support

--- radialTreemap/jsonParse.py
+++ radialTreemap/jsonParse.py
@@ -1,18 +1,33 @@
 import json
 
-with open('radial1.json') as data_file:    
+with open('radial12.json') as data_file:    
     data = json.load(data_file)
-newFile=open('radial.json','w')
-
+newFile=open('radial1.json','w')
+flag=0
+if "web.archive.org" in data[0]["url"]:
+    flag=-1
 for i in range(len(data)):
-    if i is not 0:
+    if i is not flag:
         curLink = data[i]
         curLink["referer"] =curLink["referer"][2:]
         curLink["referer"] =curLink["referer"][:len(curLink["referer"])-1]
+        if flag==-1:
+            if "https:"in curLink["referer"]:
+                curLink["referer"]=curLink["referer"][43:]
+            else:
+                curLink["referer"]=curLink["referer"][42:]
+            if "https:"in curLink["url"]:
+                curLink["url"]=curLink["url"][43:]
+            else:
+                curLink["url"]=curLink["url"][42:]
         data[i]=curLink
-data[0]=""
-temp=str(data)
-temp=temp[5:]
-temp="["+temp
+
+if flag==0:
+    data[0]=""
+    temp=str(data)
+    temp=temp[5:]
+    temp="["+temp
+else:
+    temp=str(data)
 newFile.write(temp)
 newFile.close
